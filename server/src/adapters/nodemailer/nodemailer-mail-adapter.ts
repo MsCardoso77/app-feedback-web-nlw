ce79780beaf5eb5a0e5da1ec84df1e91ff2331e3
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-05-06 14:43:31 -0300

    final updates

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6f66faae12ee07",
+    pass: "5f641bc2d0d397"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Matheus Soares <matheus@gmail.com>',
+      subject: subject,
+      html: body,
+    });
+  
+  }
+}
